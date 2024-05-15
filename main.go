548eab0e9f0eb4f0077e0103a0dccf71f99fe931
Arvind Anchi <arvind.anchi@kycdigi.com>
2024-05-15 16:32:23 +0400

    feat: Create model file and move train and predict there

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	. "markov.chains/markov"
 	. "markov.chains/tokenizer"
@@ -10,69 +9,27 @@ import (
 
 func main() {
 	d := "The first one to say the thing is a fool"
+	//	b, err := os.ReadFile("./dataset/inp.txt")
+	//	if err != nil {
+	//		panic(err)
+	//	}
+
+	// d := string(b)
+
 	t := NewTokenizer(d)
+	m := NewModel(2, t)
 
-	vc := len(*t.Vocab())
-	toks := t.Encode(d)
-
-	fmt.Printf("Vocab length = %d\n", vc)
-
-	mn1 := NewMat(vc, vc)
-	mn2 := NewMat(vc, vc)
-
-	for i := 1; i < len(toks); i++ {
-		err := mn1.Inc(int(toks[i-1]), int(toks[i]))
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	for i := 2; i < len(toks); i += 2 {
-		err := mn2.Inc(int(toks[i-2]), int(toks[i]))
-
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	m.Train(d)
 
 	prompt := "The"
 
 	fmt.Print(prompt)
 
 	for i := 0; i < 30; i++ {
-		enc := t.Encode(prompt)
-		tok := enc[len(enc)-1]
-
-		sm := NewMat(1, vc)
-		sm.Fill(0)
-
-		n1r, err := mn1.Row(int(tok))
+		s, err := m.Predict(prompt)
 		if err != nil {
-			log.Fatal(err)
+			panic(err)
 		}
-
-		n2r, err := mn2.Row(int(tok))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = sm.Sum(n1r)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		err = sm.Sum(n2r)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		p, err := mn1.Sample(int(tok))
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		s := t.Decode(p)
 
 		prompt += s
 
---
+++ markov/model.go
@@ -0,0 +1,71 @@
+package markov
+
+import (
+	. "markov.chains/tokenizer"
+)
+
+type Model struct {
+	tokenizer *Tokenizer
+
+	layers []Matrix
+}
+
+func NewModel(layerCount int, tokenizer *Tokenizer) *Model {
+	l := make([]Matrix, layerCount)
+	v := len(*tokenizer.Vocab())
+
+	for i := 0; i < layerCount; i++ {
+		l[i] = *NewMat(v, v)
+	}
+
+	return &Model{
+		tokenizer: tokenizer,
+		layers:    l,
+	}
+}
+
+func (m *Model) Train(data string) error {
+	toks := m.tokenizer.Encode(data)
+
+	for l, layer := range m.layers {
+		n := l + 1
+
+		for i := n; i < len(toks); i++ {
+			err := layer.Inc(int(toks[i-n]), int(toks[i]))
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func (m *Model) Predict(prompt string) (string, error) {
+	enc := m.tokenizer.Encode(prompt)
+	tok := enc[len(enc)-1]
+	v := len(*m.tokenizer.Vocab())
+
+	sm := NewMat(1, v)
+	sm.Fill(0)
+
+	for _, layer := range m.layers {
+		nr, err := layer.Row(int(tok))
+		if err != nil {
+			return "", err
+		}
+
+		err = sm.Sum(nr)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	p, err := sm.Sample(0)
+	if err != nil {
+		return "", err
+	}
+
+	return m.tokenizer.Decode(p), nil
+}
